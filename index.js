0aa1f029ce9c4cc9fb0b96500780a1a94a259061
IgnFed <ignaciofedorenco2317@gmail.com>
2022-02-24 00:05:23 -0300

    Finalized circular-palette

---
+++ index.js
@@ -0,0 +1,46 @@
+const ROOT = document.documentElement;
+const items = [...document.querySelectorAll('.circle')]
+const list = document.querySelector('.clickeable-circle')
+const body = document.body;
+
+let menuIsActive = false
+
+console.log(items)
+
+items.forEach((element)=>{
+  const bgColor = element.getAttribute('data-color');
+  element.style.setProperty('background-color',  bgColor)
+})
+
+
+function displayElements(){
+  items.forEach((el, idx)=>{
+    el.style.transform = `rotate(${((360/items.length)*(idx))}deg) translate(${(-(120+items.length+50))}px)`
+  })
+}
+
+function hideElements(){
+  items.forEach((el, idx)=>{
+    el.style.transform = `rotate(${((360/items.length)*(-idx))}deg) translate(0)`
+  })
+  body.style.setProperty('background-color', 'rgba(114, 17, 108, 0.849)')
+}
+
+function changeBackgrond(tgt){
+  console.log(tgt)
+}
+
+items.forEach((element)=>{
+  element.addEventListener('click', (e)=>{
+    const bgColor = getComputedStyle(e.target, null).backgroundColor
+    body.style.setProperty('background-color', bgColor)
+  })
+})
+
+list.addEventListener('click', (e)=>{
+  if(!menuIsActive) displayElements()
+  else hideElements()
+  menuIsActive = !menuIsActive 
+  e.target.classList.toggle('active', menuIsActive)
+})
+
